c9dfcf76d6427010d2449704d525c5d3fa9f9ee9
Mognia <1mognia@gmail.com>
2023-05-09 16:11:17 +0330

    render sample posts

--- next.config.js
+++ next.config.js
@@ -1,4 +1,29 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+module.exports = {
+    reactStrictMode: true,
 
-module.exports = nextConfig
+    images: {
+        loader: 'akamai',
+        path: '',
+    },
+
+    webpack: (config) => {
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            'react/jsx-runtime.js': require.resolve('react/jsx-runtime'),
+        }
+
+        config.resolve = {
+            ...config.resolve,
+
+            fallback: {
+                ...config.resolve.fallback,
+                child_process: false,
+                fs: false,
+                // 'builtin-modules': false,
+                // worker_threads: false,
+            },
+        }
+
+        return config
+    },
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,57 @@
 import Image from 'next/image'
+import {getPostsMetaData} from "../../lib/getPostsData";
+interface PostData {
+    // Define the properties and their types for a post
+    title: string;
+    description: string;
+    date: string;
+    id: string;
+}
 
-export default function Index() {
+interface IndexProps {
+    // Define the type of the postsData prop
+    postsData: PostData[];
+}
+export default function Index({postsData}: IndexProps) {
     return (
-        <>
-            <h1>HOOOME</h1>
-        </>
+        <div className = 'info-container'>
+            <img src = 'batman.png' alt = 'Batman Logo'/>
+            <p className = 'info-description'>Hi I'm Batman, the saviour of Gotham City and I like to roam in nights to bash the bad guys.</p>
+            <p className = 'info-description'>But please don't call me as a source for <b>Corona Virus</b> and it could be the <b>Joker</b> who
+                might have started this mess.</p>
+            <hr/>
+            {postsData?.map((metadata) => {
+                return (
+                    <div key = {metadata.id}>
+                        <h2 className = 'post-title'>{metadata.title}</h2>
+                        <p className = 'post-description'>{metadata.description}</p>
+                    </div>
+                )
+            })}
+            <style jsx>{`
+        .info-container {
+          margin: 0 5% 0 5%;
+        }
+
+        img {
+          width: 20%;
+          max-width: 20%;
+          height: auto;
+          margin-left: 40%;
+        }
+
+        .info-description {
+          font-size: 20px;
+        }
+      `}</style>
+        </div>
     )
+}
+export async function getStaticProps() {
+    const postsData = getPostsMetaData();
+    return {
+        props: {
+            postsData: postsData,
+        }
+    }
 }
